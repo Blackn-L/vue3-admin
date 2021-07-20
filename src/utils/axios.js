491df4c18d4fa401d5b9d3d1979a8a639e1b51b1
Blackn <nilinfeng6318@gmail.com>
2021-07-20 09:41:51 +0800

    axios增加错误提示以及更改token获取方式

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import router from '@/router/index'
 import config from '~/config'
+import { ElMessage } from 'element-plus'
+import { localGet } from './index'
 
 // 后端没有区分测试和正式接口，都写成一个接口。
 axios.defaults.baseURL = config[import.meta.env.MODE].baseUrl
@@ -15,11 +17,11 @@ axios.defaults.headers.post['Content-Type'] = 'application/json'
 // 请求拦截器，内部根据返回值，重新组装，统一管理。
 axios.interceptors.response.use(res => {
   if (typeof res.data !== 'object') {
-    console.log('服务端异常！')
+    ElMessage.error('服务端异常！')
     return Promise.reject(res)
   }
   if (res.data.resultCode != 200) {
-    if (res.data.message) console.log(res.data.message)
+    if (res.data.message) ElMessage.error(res.data.message)
     if (res.data.resultCode == 419) {
       router.push({ path: '/login' })
     }
