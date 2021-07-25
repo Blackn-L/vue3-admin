c1ebda40b4a45f54698644946f34fb27e6bb5faa
Blackn <nilinfeng6318@gmail.com>
2021-07-26 00:48:54 +0800

    update:抽离侧边栏

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,7 @@
   <div class="layout">
     <el-container class="container">
       <!-- 侧边栏 -->
-      <el-aside class="aside">
-        <div class="head">
-          <span>Vue3 Admin</span>
-        </div>
-        <div class="line"></div>
-        <el-menu background-color="#222832" text-color="#fff" router>
-          <el-submenu index="1">
-            <template #title>
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="/"
-                ><i class="el-icon-data-line" />首页</el-menu-item
-              >
-              <el-menu-item index="/add"
-                ><i class="el-icon-data-line" />添加商品</el-menu-item
-              >
-            </el-menu-item-group>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
+      <Sider />
       <!-- 内容区 -->
       <el-container class="content">
         <Header />
@@ -38,10 +17,10 @@
 
 <script setup>
 import Header from "@/components/Header.vue";
+import Sider from "@/components/Sider.vue";
 import Footer from "@/components/Footer.vue";
 name: "App";
 // vue
-// import HelloWorld from "./components/HelloWorld.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
@@ -59,28 +38,6 @@ body {
 
 .container {
   height: 100vh;
-}
-
-.aside {
-  min-height: 100vh;
-  width: 200px !important;
-  background-color: #222832;
-}
-
-.head {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-}
-.head span {
-  color: #ffffff;
-  font-size: 20px;
-}
-
-.line {
-  border-top: 1px solid hsla(0, 0%, 100%, 0.05);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 }
 
 .content {
@@ -102,17 +59,6 @@ body {
   margin: 0;
   border: 0;
   box-sizing: border-box;
-}
-.el-menu {
-  border-right: none !important;
-}
-.el-submenu {
-  border-top: 1px solid hsla(0, 0%, 100%, 0.05);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-}
-a {
-  color: #409eff;
-  text-decoration: none;
 }
 .el-pagination {
   text-align: center;
---
+++ src/components/Sider.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- 侧边栏 -->
+  <el-aside class="aside">
+    <div class="head">
+      <span>Vue3 Admin</span>
+    </div>
+    <div class="line"></div>
+    <el-menu background-color="#222832" text-color="#fff" router>
+      <el-submenu index="1">
+        <template #title>
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/"
+            ><i class="el-icon-data-line" />首页</el-menu-item
+          >
+          <el-menu-item index="/add"
+            ><i class="el-icon-data-line" />添加商品</el-menu-item
+          >
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: "Sider",
+};
+</script>
+
+<style scoped>
+.aside {
+  min-height: 100vh;
+  width: 200px !important;
+  background-color: #222832;
+}
+
+.head {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+}
+.head span {
+  color: #ffffff;
+  font-size: 20px;
+}
+
+.line {
+  border-top: 1px solid hsla(0, 0%, 100%, 0.05);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
+<style >
+.el-menu {
+  border-right: none !important;
+}
+.el-submenu {
+  border-top: 1px solid hsla(0, 0%, 100%, 0.05);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
