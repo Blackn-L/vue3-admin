dbb14459e7a40906f6bab0862aa7bf2b1146b951
Blackn <nilinfeng6318@gmail.com>
2021-08-05 06:12:32 +0800

    update:增加三个页面配置

---
+++ src/views/IndexConfig.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-card class="index-container"></el-card>
+</template>
+
+<script>
+import { onUnmounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+export default {
+  name: "IndexConfig",
+  setup() {
+    const router = useRouter();
+    // 监听路由变化
+    const unWatch = router.beforeEach((to) => {
+      console.log(to.name);
+    });
+    // 销毁时候卸载路由守卫，防止多个路由守卫运行
+    onUnmounted(() => {
+      unWatch();
+    });
+  },
+};
+</script>
+
+<style>
+</style>
