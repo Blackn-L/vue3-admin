0d7c59085b5dfa04709d284cf1f5d8947938c4c0
Blackn <nilinfeng6318@gmail.com>
2021-08-11 09:23:55 +0800

    update

--- src/views/IndexConfig.vue
+++ src/views/IndexConfig.vue
@@ -1,5 +1,54 @@
 <template>
-  <el-card class="index-container"></el-card>
+  <el-card class="index-container">
+    <el-table
+      :load="loading"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+    >
+      <el-table-column prop="configName" label="商品名称"> </el-table-column>
+      <el-table-column label="跳转链接">
+        <template #default="scope">
+          <a target="_blank" :href="scope.row.redirectUrl">{{
+            scope.row.redirectUrl
+          }}</a>
+        </template>
+      </el-table-column>
+      <el-table-column prop="configRank" label="排序值" width="120">
+      </el-table-column>
+      <el-table-column prop="goodsId" label="商品编号" width="200">
+      </el-table-column>
+      <el-table-column prop="createTime" label="添加时间" width="200">
+      </el-table-column>
+      <el-table-column label="操作" width="100">
+        <template #default="scope">
+          <a
+            style="cursor: pointer; margin-right: 10px"
+            @click="handleEdit(scope.row.configId)"
+            >修改</a
+          >
+          <el-popconfirm
+            title="确定删除吗？"
+            confirmButtonText="确定"
+            cancelButtonText="取消"
+            @confirm="handleDeleteOne(scope.row.configId)"
+          >
+            <template #reference>
+              <a style="cursor: pointer">删除</a>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--总数超过一页，再展示分页器-->
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="pageSize"
+      :current-page="currentPage"
+      @current-change="changePage"
+  /></el-card>
 </template>
 
 <script>
